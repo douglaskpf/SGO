aa305854aea41d0d675e6aaedc64e8c1d1a6fcb9
douglas <douglas@douglask>
2017-04-18 10:33:32 -0300

    Ajustes 18/04

--- src/java/br/edu/ifsul/controle/ControleInsumo.java
+++ src/java/br/edu/ifsul/controle/ControleInsumo.java
@@ -2,17 +2,13 @@ package br.edu.ifsul.controle;
 
 import br.edu.ifsul.dao.InsumoDAO;
 import br.edu.ifsul.dao.GrupoDAO;
-import br.edu.ifsul.dao.ServicoDAO;
 import br.edu.ifsul.modelo.Insumo;
 import br.edu.ifsul.modelo.Grupo;
-import br.edu.ifsul.modelo.Servico;
-import br.edu.ifsul.modelo.ServicoInsumo;
 import br.edu.ifsul.util.Util;
 import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
 
 @Named(value = "controleInsumo")
 @SessionScoped
@@ -22,19 +18,10 @@ public class ControleInsumo implements Serializable {
     private InsumoDAO<Insumo> dao;
     private Insumo objeto;
     private Boolean editando;
-    
-      
-    //testes
-    @EJB
-    private ServicoDAO<Servico> daoServico;
-    private Servico objetoServico;
-    private ServicoInsumo item;
-     
-      
+         
     @EJB
     private GrupoDAO<Grupo> daoGrupo;
-             
-    
+         
     public ControleInsumo() {
         editando = false;
     }
@@ -77,50 +64,7 @@ public class ControleInsumo implements Serializable {
         }
     }  
   
-   /*public void atualizarInsumo(Integer id){
    
-           try{
-              ServicoInsumo si = null;    
-              Insumo in = null;
-              Servico s = null;
-                             
-              //pegando todos os ServicoInsumo que possuem o Insumo
-           List<ServicoInsumo> lista;
-                lista = em.createQuery("from ServicoInsumo  where insumo.id = '" + id +"'").getResultList();
-               
-                
-                //Atualizando o valor total do ServicoInsumo
-               for(ServicoInsumo si : lista){
-                   si.setValorUnitario(in.getPreco());
-                   si.getValorTotal()= (si.getValorUnitario() * si.getQuantidade());
-                   em.merge(si);
-                   
-               }    
-               
-               //Atualizando o valorServico do Servico
-                    for (ServicoInsumo si : lista){
-                       Servico s = si.getServico();
-                        s.setValorServico(0.0);
-                      
-                        for (ServicoInsumo si2 : s.getItens()){
-                            s.setValorServico(si2.setValorTotal());
-                                          
-                        
-                        }
-                        
-                    }    
-                    
-         
-                  
-    } catch (Exception e) {
-            Util.mensagemErro("Erro ao atualizar o valor do insumo: " + Util.getMensagemErro(e));
-        }
- 
-       }
-   
-       */
-       
-    
     public void salvar() {
         try {
             if (objeto.getId() == null) {
@@ -140,7 +84,7 @@ public class ControleInsumo implements Serializable {
         //após atualizar a classe "servicoInsumo" mandar o "novo" total para o valor_servico em "Servico"//
             
         
-      //  atualizarInsumo();
+         //dao.atualizarInsumo();
    
     }     
         
@@ -175,29 +119,5 @@ public class ControleInsumo implements Serializable {
     public void setDaoGrupo(GrupoDAO<Grupo> daoGrupo) {
         this.daoGrupo = daoGrupo;
     }
-
-    public ServicoInsumo getItem() {
-        return item;
-    }
-
-    public void setItem(ServicoInsumo item) {
-        this.item = item;
-    }
-
-    public ServicoDAO<Servico> getDaoServico() {
-        return daoServico;
-    }
-
-    public void setDaoServico(ServicoDAO<Servico> daoServico) {
-        this.daoServico = daoServico;
-    }
-
-    public Servico getObjetoServico() {
-        return objetoServico;
-    }
-
-    public void setObjetoServico(Servico objetoServico) {
-        this.objetoServico = objetoServico;
-    }
-   
+  
 }
--- src/java/br/edu/ifsul/dao/InsumoDAO.java
+++ src/java/br/edu/ifsul/dao/InsumoDAO.java
@@ -1,22 +1,44 @@
-
 package br.edu.ifsul.dao;
 
 import br.edu.ifsul.modelo.Insumo;
+import br.edu.ifsul.modelo.Servico;
+import br.edu.ifsul.modelo.ServicoInsumo;
 import java.io.Serializable;
+import java.util.List;
 import javax.ejb.Stateful;
 
 @Stateful
-public class InsumoDAO<T> extends DAOGenerico<Insumo>implements Serializable {
+public class InsumoDAO<T> extends DAOGenerico<Insumo> implements Serializable {
 
-    public InsumoDAO(){
+    public InsumoDAO() {
         super();
         super.setClassePersistente(Insumo.class);
         super.setOrdem("nome");// ordem padrão
     }
 
-    
-    
-    //fazer a função aqui
-    
-    
+    /*
+    public void atualizarInsumo(Integer id) throws Exception {
+
+        Insumo in = null;
+                  //pegando todos os ServicoInsumo que possuem o Insumo
+        List<ServicoInsumo> lista;
+        lista = super.getEm().createQuery("from ServicoInsumo where insumo.id = '" + id + "'").getResultList();
+
+        //Atualizando o valor total do ServicoInsumo
+        for (ServicoInsumo si : lista) {
+            si.valorUnitario = in.preco;
+            si.valorTotal = si.valorUnitario * si.quantidade;
+            em.merge(si);
+        }
+        //Atualizando o valorServico do Servico
+        for (ServicoInsumo si : lista) {
+            Servico s = si.servico;
+            s.valorServico = 0.0;
+
+            for (ServicoInsumo si2 : s.itens) {
+                s.valorServico += si2.valorTotal;
+            }
+            em.merge(s);
+        }
+    }  */
 }
