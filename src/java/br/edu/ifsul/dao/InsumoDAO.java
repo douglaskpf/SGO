f91621365a80a211e20fbb8142a23cc3cc2e4316
douglas <douglas@douglask>
2017-04-24 21:27:45 -0300

    Ajustes 24/04

--- src/java/br/edu/ifsul/dao/InsumoDAO.java
+++ src/java/br/edu/ifsul/dao/InsumoDAO.java
@@ -16,29 +16,32 @@ public class InsumoDAO<T> extends DAOGenerico<Insumo> implements Serializable {
         super.setOrdem("nome");// ordem padrão
     }
 
-    /*
     public void atualizarInsumo(Integer id) throws Exception {
 
         Insumo in = null;
-                  //pegando todos os ServicoInsumo que possuem o Insumo
+        Double aux = null; //váriável para pegar o novo valor do serviço
+        
+        //pegando todos os ServicoInsumo que possuem o Insumo
         List<ServicoInsumo> lista;
         lista = super.getEm().createQuery("from ServicoInsumo where insumo.id = '" + id + "'").getResultList();
 
         //Atualizando o valor total do ServicoInsumo
         for (ServicoInsumo si : lista) {
-            si.valorUnitario = in.preco;
-            si.valorTotal = si.valorUnitario * si.quantidade;
-            em.merge(si);
+            si.setValorUnitario(in.getPreco());
+            si.setValorTotal(si.getValorUnitario() * si.getQuantidade());
+            super.getEm().merge(si);
         }
+        
         //Atualizando o valorServico do Servico
         for (ServicoInsumo si : lista) {
-            Servico s = si.servico;
-            s.valorServico = 0.0;
+            Servico s = si.getServico();
+            s.setValorServico(0.0);
 
-            for (ServicoInsumo si2 : s.itens) {
-                s.valorServico += si2.valorTotal;
+            for (ServicoInsumo si2 : s.getItens()) {
+                aux += si2.getValorTotal();
             }
-            em.merge(s);
+            s.setValorServico(aux);
+            super.getEm().merge(s);
         }
-    }  */
+    }
 }
