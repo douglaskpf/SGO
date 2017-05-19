e3581200a4908aca23e628945d636a4d91b333e8
douglas <douglas@douglask>
2017-05-19 15:16:36 -0300

    AJUSTES 19/05

--- src/java/br/edu/ifsul/dao/InsumoDAO.java
+++ src/java/br/edu/ifsul/dao/InsumoDAO.java
@@ -17,27 +17,21 @@ public class InsumoDAO<T> extends DAOGenerico<Insumo> implements Serializable {
     }
 
     public void atualizarInsumo(Insumo in) throws Exception {
- 
-    
         Double aux = null; //váriável para pegar o novo valor do serviço
-
         //pegando todos os ServicoInsumo que possuem o Insumo
         List<ServicoInsumo> lista;
         lista = super.getEm().createQuery("from ServicoInsumo where insumo.id = '" + in.getId() + "'").getResultList();
-
         //Atualizando o valor total do ServicoInsumo
         for (ServicoInsumo si : lista) {
             si.setValorUnitario(in.getPreco());
             si.setValorTotal(si.getValorUnitario() * si.getQuantidade());
             super.getEm().merge(si);
         }
-
         //Atualizando o valorServico do Servico
         for (ServicoInsumo si : lista) {
             aux = 0.0;
             Servico s = si.getServico();
             s.setValorServico(0.0);
-
             for (ServicoInsumo si2 : s.getItens()) {
                 aux += si2.getValorTotal();
             }
