e9ee88f596ccda2eaa6c49adf63e53f4697a3ef0
douglas <douglas@douglask>
2017-06-16 20:11:47 -0300

    Ajustes 16-06

--- src/java/br/edu/ifsul/dao/CidadeDAO.java
+++ src/java/br/edu/ifsul/dao/CidadeDAO.java
@@ -13,7 +13,6 @@ public class CidadeDAO<T> extends DAOGenerico<Cidade> implements Serializable {
         super.setClassePersistente(Cidade.class);
         super.setOrdem("nome");        
     }
-    
-    
+       
     
 }
